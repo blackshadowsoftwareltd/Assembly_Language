339dc8e58f5d68b75ea3a65a0316ec99b3054aad
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-05-25 18:00:04 +0600

    get user input with proper message

--- encryption_decryption.asm
+++ encryption_decryption.asm
@@ -1,17 +1,30 @@
-display_string macro message
-    local msg_start
-    mov dx, offset message    ; Load address of the message
-    mov ah, 09h               ; DOS function: print string
-    int 21h                   ; Call DOS interrupt
-endm
+DISPLAY_MSG MACRO msg
+    lea dx, msg
+    mov ah, 09h
+    int 21h
+ENDM
+
+NEW_LINE MACRO
+    mov dl, 0Dh    ; Carriage return
+    mov ah, 02h    ; DOS function: display character
+    int 21h        ; Call DOS interrupt
+
+    mov dl, 0Ah    ; Line feed
+    int 21h        ; Call DOS interrupt
+ENDM
 
 name "crypt"
 org 100h
 
 jmp start
 
+msg_input db 'Enter a string : $'
+msg_inserted db 0Dh, 0Ah, 'You Inserted : $'
 msg_enc db 'Encrypted : $' 
 msg_dec db 'Decrypted : $' 
+
+input_buffer db 100, 0
+input_string db 100 dup('$')
 encrypted db 'hello world!', 0Dh,0Ah, '$' 
 decrypted db  'axpps gsupn!', 0Dh,0Ah, '$' 
 table1 db 97 dup (' '), 'klmnxyzabcopqrstvuwdefghij'
@@ -20,6 +33,7 @@ table2 db 97 dup (' '), 'hijtuvwxyzabcdklmnoprqsefg'
 
 
 start: 
+call user_input
 call encrypt
 call decrypt
 
@@ -29,8 +43,8 @@ lea si, encrypted
 call parse
 
 ; show result:
-display_string  msg_enc
-display_string  encrypted
+DISPLAY_MSG  msg_enc
+DISPLAY_MSG  encrypted
 
 decrypt:
 lea bx, table2
@@ -38,12 +52,36 @@ lea si, decrypted
 call parse
 
 ; show result:
-display_string msg_dec
-display_string decrypted
+DISPLAY_MSG msg_dec
+DISPLAY_MSG decrypted
 
 ; wait for any key...
 mov ah, 0
 int 16h
+
+; -------- take user input to encryption {
+	user_input:
+    DISPLAY_MSG msg_input
+	call read_input
+	DISPLAY_MSG input_string
+	NEW_LINE
+	ret
+; ---------take user input to encryption }
+
+; -------- user input {
+read_input:
+    mov ah, 0Ah        
+    lea dx, input_buffer 
+    int 21h           
+    mov si, dx          
+    add si, 2
+	DISPLAY_MSG msg_inserted
+
+ 
+    mov di, offset input_string   
+    mov cx, 100      
+    rep movsb 
+; -------- user input }
 
 ret   ; exit to operating system.
 
